ad04ba9b3608318e31aae54f1e9c7e87c0090054
cristianp98 <cristian.panche12@gmail.com>
2024-09-09 18:13:35 +0200

    Cambios de inicio

--- gestion_restaurantes/urls.py
+++ gestion_restaurantes/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('administracion/', include('administracion.urls')),
+    path('', include('administracion.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
